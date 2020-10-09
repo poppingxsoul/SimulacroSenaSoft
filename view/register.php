bdbc2c5d9f2c4a8661444fbd9ae46fe97f347970
PoppingxSoul <jaramirez547@misena.edu.co>
2020-10-09 01:20:34 -0500

    Vistas

--- view/register.php
+++ view/register.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Iniciar sesión</title>
+    <title>Registrarse</title>
 </head>
 
 <body>
@@ -32,39 +32,39 @@
         </nav>
     </header>
 
-    <div class="container" style="max-width: 1000px">
+    <div class="container mb-5" style="max-width: 1000px">
         <div class="container-fluid bg-white border shadow text-center">
-            <div class="row">
-                <div class="col-sm-4 centrar">
-                    <div class="container">
-                        <img src="../assets/img/logo.png" width="80px" alt="Imagen fondo">
-                        <h6 class="text-white pt-3">SERVICIO NACIONAL DE APRENDIZAJE</h6>
-                        <span class="text-white">SENASOFT</span>
-                    </div>
-                </div>
-
-                <div class="col-sm-8 p-5">
-                    <h4 class="text-muted pb-4">Iniciar sesión</h4>
-                    <div class="form-group">
-                        <input type="number" class="form-control p pb-4" placeholder="Usuario">
-                    </div>
+            <div class="container p-5">
+                <h4 class="text-muted">Registrarse</h4>
+                <hr>
+                <form action="" method="POST">
                     <div class="form-group pt-1">
-                        <input type="password" class="form-control p pb-4" placeholder="Contraseña">
+                        <input type="text" class="form-control p pb-4" name="nombres" placeholder="Nombres completos" required="">
+                    </div>
+                    <div class="form-group pt-2">
+                        <input type="email" class="form-control p pb-4" name="email" placeholder="Email" required="">
+                    </div>
+                    <div class="form-group pt-2">
+                        <input type="number" class="form-control p pb-4" name="identificacion" placeholder="Numero de identificación" required="">
+                    </div>
+                    <div class="form-group pt-2">
+                        <input type="password" class="form-control p pb-4" name="pass" placeholder="Contraseña" required="">
                     </div>
                     <div class="form-group pt-3">
-                        <input type="submit" class="form-control btn btn-info" value="Iniciar sesión">
+                        <input type="submit" class="form-control btn btn-info" value="Registrarse">
                     </div>
-                    <div class="form-group pt-3">
-                        <span class="text-dark">¿No tienes una cuenta?</span>
-                        <a href="register.php">Registrate</a>
+                    <div class="form-group pt-3 pb-0 mb-4">
+                        <span class="text-dark">¿Tienes una cuenta?</span>
+                        <a href="index.php">Inicia sesión</a>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
