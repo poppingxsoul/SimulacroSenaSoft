9b85a38981d911eb28a1dfb7a5f2c2ffdf8adb3b
PoppingxSoul <jaramirez547@misena.edu.co>
2020-10-09 16:45:59 -0500

    rutas issets

--- view/admin/menu.php
+++ view/admin/menu.php
@@ -1,7 +1,7 @@
 <header>
     <div class="container-fluid mb-5">
         <nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
-            <a class="nav-link" href="index.php"><img src="../../assets/img/logosoft.png" alt="Logo" width="100px"></a>
+            <a class="nav-link" href="index.php"><img src="assets/img/logosoft.png" alt="Logo" width="100px"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
